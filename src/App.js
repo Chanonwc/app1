e7a67bfda1a5906c738736b30eef3aaa00a84134
Chanonwc <wc.chanon@gmail.com>
2026-07-13 10:55:25 +0700

    Update app.js with insert variable

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let r = 'react';
+    let rn ='react native';
+    const el= (
+      <div>
+        create Web & Mobile Apps<br/>
+        with {r} & {rn}
+      </div>
+
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    el
+  
+    );
+  }
 
 export default App;
