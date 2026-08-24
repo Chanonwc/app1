22a06d32d9452c2f9d443c684c49697a0d6e6f89
Chanonwc <wc.chanon@gmail.com>
2026-08-24 10:52:43 +0700

    create Context-Data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import Effect from './effect';
+import React from 'react'
+import { userContext } from './context'
+import Header from './context-header2'
+import Content from './context-content2'
 
+export default function App() {
+    let [user, setUser] = React.useState('')
 
-function App() {
-  
-  return <Effect/>
-
+    return (
+        <userContext.Provider value={[user, setUser]}>
+            <Header/>
+            <Content/>
+        </userContext.Provider>
+    )
 }
-export default App;
