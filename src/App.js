64f5bb0f52a0bfcad8ee5f2c47cf796e7faafb23
Chanonwc <wc.chanon@gmail.com>
2026-09-07 11:03:46 +0700

    make Routes to 4 page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react'
-import { userContext } from './context'
-import Header from './context-header2'
-import Content from './context-content2'
+import Router3 from './router3'
 
 export default function App() {
-    let [user, setUser] = React.useState('')
-
-    return (
-        <userContext.Provider value={[user, setUser]}>
-            <Header/>
-            <Content/>
-        </userContext.Provider>
-    )
+    return <Router3/>
 }
